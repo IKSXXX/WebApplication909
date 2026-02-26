aad46a1e2f3db78fe5beb273da3710f1bcb49db9
Anon <Anon@DESKTOP-DTLM9AJ>
2026-02-26 19:20:40 +0300

    Serilog

--- WebApplication909/Program.cs
+++ WebApplication909/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Localization;
+using Serilog;
 using System.Globalization;
 using WebApplication909;
 using WebApplication909.Interfaces;
 using WebApplication909.Repositories;
+using Serilog;
 
 namespace OnlineShopWebApp
 {
@@ -10,41 +12,59 @@ namespace OnlineShopWebApp
     {
         public static void Main(string[] args)
         {
+            
+
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            Log.Logger = new LoggerConfiguration()
+                    .CreateLogger(); //инициализация глобального логера Serilog с базовой конфигурацией
 
-            builder.Services.AddSingleton<IProductsRepository, InMemoryProductsRepository>();
-            builder.Services.AddSingleton<ICartsRepository, InMemoryCartsRepository>();
-            builder.Services.AddSingleton<IOrdersRepository, InMemoryOrdersRepository>();
-            builder.Services.AddSingleton<IFavoritesRepository, InMemoryFavouritesRepository>();
-            builder.Services.AddSingleton<IComparisonsRepository, InMemoryComparisonsRepository>();
-            builder.Services.AddSingleton<IRolesRepository, InMemoryRolesRepository>();
-            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            try //начало блока для обработки ошибок запуска приложения
             {
-                var supportedCultures = new[]
+                Log.Information("Starting server..."); //запись информационного сообщения о запуске сервера
+
+                builder.Host.UseSerilog((context, loggerConfiguration) =>
                 {
-                    new CultureInfo("en-US")
-                };
-                options.DefaultRequestCulture = new RequestCulture("en-US");
-                options.SupportedCultures = supportedCultures;
-                options.SupportedUICultures = supportedCultures;
-            });
+                    loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+                }); //подключение Serilog в качестве службы логирования по умолчанию, конфигурация настроек читается 
+                    //из файла конфигурации
 
-            var app = builder.Build();
 
-            app.UseHttpsRedirection();
+                // Add services to the container.
+                builder.Services.AddControllersWithViews();
 
-            app.UseRequestLocalization();
+                builder.Services.AddSingleton<IProductsRepository, InMemoryProductsRepository>();
+                builder.Services.AddSingleton<ICartsRepository, InMemoryCartsRepository>();
+                builder.Services.AddSingleton<IOrdersRepository, InMemoryOrdersRepository>();
+                builder.Services.AddSingleton<IFavoritesRepository, InMemoryFavouritesRepository>();
+                builder.Services.AddSingleton<IComparisonsRepository, InMemoryComparisonsRepository>();
+                builder.Services.AddSingleton<IRolesRepository, InMemoryRolesRepository>();
 
-            app.UseRouting();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                var app = builder.Build();
 
-            app.Run();
+                app.UseSerilogRequestLogging(); //замена логирования, используемого по умолчанию в ASP.NET Core, 
+                                                //на ведение журнала запросов Serilog
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                app.Run();
+            }
+            catch (Exception ex) //обработка ошибок запуска: логируется критическая ошибка при аварийном завершении, а затем закрываются и очищаются все логи
+            {
+                Log.Fatal(ex, "server terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
