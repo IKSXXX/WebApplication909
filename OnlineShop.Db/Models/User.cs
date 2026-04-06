7db9a39c76f54ee96f354fbb9135325d7789e4f7
Anon <Anon@DESKTOP-DTLM9AJ>
2026-04-06 18:05:03 +0300

    установил пакеты для identity

---
+++ OnlineShop.Db/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OnlineShop.Db.Models
+{
+    internal class User : IdentityUser
+    {
+
+    }
+}
